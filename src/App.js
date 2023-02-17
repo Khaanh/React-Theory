33fd67fee5491f85d3e064f25fc8ff5be17a24ef
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2023-02-17 07:34:33 +0100

    JSX tutorial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+const style = {
+  color: "red",
+  fontSize: "36px",
+  backgroundColor: "green",
+  padding: "18px",
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 style={style} className="h1-title">
+        Hello app-react !
+      </h1>
+    </>
   );
 }
 
