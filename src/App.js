a83f3ef5132f863b2ef39b63bbdd87c3f49d1508
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2023-02-20 22:48:16 +0100

    State

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
+// class Welcome extends React.Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       count: 0,
+//       date: new Date(),
+//     };
+
+//     setInterval(() => {
+//       this.setState((state, props) => ({
+//         date: new Date(),
+//         count: state.count + 1,
+//       }));
+//     }, 1000);
+//   }
+
+//   render() {
+//     return (
+//       <>
+//         <p>{this.state.count}</p>
+//         <h1>{this.state.date.toLocaleString()}</h1>;
+//       </>
+//     );
+//   }
+// }
+
+function Welcome(props) {
+  let [date, setDate] = useState({
+    date: new Date(),
+  });
+
+  setInterval(() => {
+    setDate({
+      date: new Date(),
+    });
+  }, 100);
+
+  return <h1>{date.date.toLocaleString()}</h1>;
+}
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Welcome />;
 }
 
 export default App;
