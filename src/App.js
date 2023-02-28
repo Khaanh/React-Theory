45de010b26968d84d718ef45d0bd12c395792baa
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2023-02-28 09:17:17 +0100

    useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,42 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
-import City from "./components/City";
-import CitiesList from "./components/CitiesList";
+// import City from "./components/City";
+// import CitiesList from "./components/CitiesList";
 
 function App() {
-  const [cities, setCities] = useState([
-    { name: "Киев", description: "Столица Украины" },
-    { name: "Харьков", description: "Первая столица Украины" },
-  ]);
-  const [indexCurrent, setIndexcurrent] = useState(0);
+  const [users, setUsers] = useState([]);
+  const [search, setSeatch] = useState("");
 
-  const handlerChangeCity = (n, description) => {
-    setCities(
-      cities.map((city, index) => {
-        if (index === n) {
-          return {
-            ...city,
-            description,
-          };
-        }
-        return city;
-      })
-    );
-  };
+  useEffect(() => {
+    fetch("/users?search=" + search)
+      .then((response) => response.json())
+      .then((users) => setUsers(users));
+  }, [search]);
 
-  const handlerSelectCity = (index) => setIndexcurrent(index);
-
-  return (
-    <>
-      <City
-        cities={cities}
-        onChangeCity={handlerChangeCity}
-        indexCurrent={indexCurrent}
-      />
-      <CitiesList cities={cities} onSelectCity={handlerSelectCity} />
-    </>
-  );
+  return <p>Hello useEffect!</p>;
 }
 
 export default App;
