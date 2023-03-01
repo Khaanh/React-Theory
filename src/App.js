ef51b0078d7a8e7cbf235f728c6d963d634fc17c
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2023-03-01 08:29:05 +0100

    useMemo and useCallBack

--- src/App.js
+++ src/App.js
@@ -1,20 +1,64 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 import "./App.css";
 
 // import City from "./components/City";
 // import CitiesList from "./components/CitiesList";
 
+// function createUser(name, surname) {
+//   const user = { name, surname };
+
+//   console.log(user);
+
+//   return user;
+// }
+
+// function App() {
+//   const [name, setName] = useState("");
+//   const [surname, setSurname] = useState("");
+//   const [counter, setCounter] = useState(0);
+//   const user = useMemo(() => createUser(name, surname), [name, surname]);
+
+//   return (
+//     <div>
+//       <button onClick={() => setCounter(counter + 1)}>
+//         На меня нажали {counter} раз
+//       </button>
+//       <br />
+//       <input
+//         type="text"
+//         value={name}
+//         onChange={(e) => setName(e.target.value)}
+//       />
+//       <br />
+//       <input
+//         type="text"
+//         value={surname}
+//         onChange={(e) => setSurname(e.target.value)}
+//       />
+//       <br />
+
+//       <pre>{JSON.stringify(user, null, 2)}</pre>
+//     </div>
+//   );
+// }
+
 function App() {
-  const [users, setUsers] = useState([]);
-  const [search, setSeatch] = useState("");
+  const [message, setMessage] = useState("Всем привет");
+  const [counter, setCounter] = useState(0);
+
+  const greeting = useCallback((text) => {
+    console.log(text);
+  }, []);
 
   useEffect(() => {
-    fetch("/users?search=" + search)
-      .then((response) => response.json())
-      .then((users) => setUsers(users));
-  }, [search]);
+    greeting(message);
+  }, [greeting, message]);
 
-  return <p>Hello useEffect!</p>;
+  return (
+    <button onClick={() => setCounter(counter + 1)}>
+      На меня нажали {counter} раз
+    </button>
+  );
 }
 
 export default App;
