ebc9e47d1c5804cc17f02af2f8c1fd947fe822ce
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2023-03-02 23:21:00 +0100

    useRef()

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React, { useReducer, useState, useContext } from "react";
-import ActionsCard from "./component/ActionsCard";
-import StatusCard from "./component/StatusCard";
-import Context from "./component/Context";
+import React, { useState, useRef, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const count = (n) => setCounter(counter + n);
-  const value = {
-    counter,
-    count,
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
+  const nameInputRef = useRef();
+  const surnameInputRef = useRef();
+
+  const handlerKeyUp1 = (e) => {
+    if (e.key === "Enter") {
+      surnameInputRef.current.focus();
+    }
+  };
+  const handlerKeyUp2 = (e) => {
+    if (e.key === "Enter") {
+      nameInputRef.current.focus();
+    }
   };
 
   return (
-    <Context.Provider value={value}>
-      <div className="container">
-        <StatusCard />
-        <ActionsCard />
-      </div>
-    </Context.Provider>
+    <form>
+      <input
+        ref={nameInputRef}
+        type="text"
+        placeholder="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        onKeyUp={handlerKeyUp1}
+      />
+      <br />
+      <input
+        ref={surnameInputRef}
+        type="text"
+        placeholder="surname"
+        value={surname}
+        onChange={(e) => setSurname(e.target.value)}
+        onKeyUp={handlerKeyUp2}
+      />
+    </form>
   );
 }
 
