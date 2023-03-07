746bd13f221a6b2e557e4fa9a1d8042fdee16b67
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2023-03-07 23:04:28 +0100

    Form

--- src/App.js
+++ src/App.js
@@ -3,17 +3,38 @@ import Mouse from "./component/Mouse";
 import Card from "./component/Card";
 import "./App.css";
 
+const ENTER = "Enter";
+
 function App() {
   const ref = useRef();
-
-  const handlerClick = useCallback(() => {
+  const clickHandler = useCallback((e) => {
     console.log(ref.current.value);
-  }, [ref]);
+  });
+
+  const keyUpHandler = useCallback((e) => {
+    const { key } = e;
+    if (key === ENTER) {
+      clickHandler();
+    }
+  });
+
+  useEffect(
+    (e) => {
+      if (ref.current) {
+        ref.current.addEventListener("keyup", keyUpHandler);
+
+        return () => {
+          ref.current.removeEventListener("keyup", keyUpHandler);
+        };
+      }
+    },
+    [keyUpHandler]
+  );
 
   return (
     <div>
       <input ref={ref} />
-      <button onClick={handlerClick}>Отправить</button>
+      <button onClick={clickHandler}>Send</button>
     </div>
   );
 }
